0b0c9fdadb1ab4a423117e155af3ab46d2f9f5c2
JRSkates <skatesproduction@gmail.com>
2023-04-03 14:25:53 +0100

    FizzBuzz 3 Tests Passing

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,11 @@
-def fizzbuzz(number)
-    'fizz'
+def fizzbuzz(num)
+  if num % 15 == 0
+    "fizzbuzz"
+  elsif num % 5 == 0 
+    "buzz"
+  elsif num % 3  == 0
+    "fizz"   
+  else
+   num.to_s
+  end
 end
